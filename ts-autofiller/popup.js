0df330a7aaa38c7d04434095d269cbc2b05adff1
fmajor <fmajor@griddynamics.com>
2025-05-23 12:28:11 +0200

    Add overtimeType and compensationType

--- ts-autofiller/popup.js
+++ ts-autofiller/popup.js
@@ -19,6 +19,9 @@ async function runAutoFill() {
             task: document.getElementById("task").value,
             timeType: document.getElementById("timeType").value,
             hours: document.getElementById("hours").value,
+            notes: document.getElementById("notes").value,
+            overtimeType: document.getElementById("overtimeType").value,
+            compensationType: document.getElementById("compensationType").value,
             administrativeTime: document.getElementById("administrativeTime").value,
         }]
     })
@@ -29,4 +32,100 @@ async function runAutoFill() {
     })
 }
 
-document.getElementById("fill-button").addEventListener("click", runAutoFill);
+window.onload = function() {
+    document.getElementById("fill-button").addEventListener("click", runAutoFill);
+    document.getElementById("fill-button").addEventListener("click", (event) => {
+        chrome.storage.sync.set({
+            projectPrefix: projectPrefix.value,
+            task: task.value,
+            timeType: timeType.value,
+            hours: hours.value,
+            notes: notes.value,
+            overtimeType: overtimeType.value,
+            compensationType: compensationType.value,
+            administrativeTime: administrativeTime.value
+        })
+    });
+
+    chrome.storage.sync.get('projectPrefix', function(data) {
+        if (typeof data.projectPrefix === 'undefined') {
+            projectPrefix.value = "Google"
+        } else {
+            projectPrefix.value = data.projectPrefix
+        }
+    })
+    chrome.storage.sync.get('task', function(data) {
+        if (typeof data.task === 'undefined') {
+            task.value = "Billable"
+        } else {
+            task.value = data.task
+        }
+    })
+    chrome.storage.sync.get('timeType', function(data) {
+        if (typeof data.timeType === 'undefined') {
+            timeType.value = "Offshore"
+        } else {
+            timeType.value = data.timeType
+        }
+    })
+    chrome.storage.sync.get('hours', function(data) {
+        if (typeof data.hours === 'undefined') {
+            hours.value = 8
+        } else {
+            hours.value = data.hours
+        }
+    })
+    chrome.storage.sync.get('notes', function(data) {
+        if (typeof data.notes === 'undefined') {
+            notes.value = ""
+        } else {
+            notes.value = data.notes
+        }
+    })
+    chrome.storage.sync.get('overtimeType', function(data) {
+        if (typeof data.overtimeType === 'undefined') {
+            overtimeType.value = "None"
+        } else {
+            overtimeType.value = data.overtimeType
+        }
+    })
+    chrome.storage.sync.get('compensationType', function(data) {
+        if (typeof data.compensationType === 'undefined') {
+            compensationType.value = "None"
+        } else {
+            compensationType.value = data.compensationType
+        }
+    })
+    chrome.storage.sync.get('administrativeTime', function(data) {
+        if (typeof data.administrativeTime === 'undefined') {
+            administrativeTime.value = "None"
+        } else {
+            administrativeTime.value = data.administrativeTime
+        }
+    })
+    
+    projectPrefix.addEventListener("change", (event) => {
+        chrome.storage.sync.set({ projectPrefix: projectPrefix.value })
+    })
+    task.addEventListener("change", (event) => {
+        chrome.storage.sync.set({ task: task.value })
+    })
+    timeType.addEventListener("change", (event) => {
+        chrome.storage.sync.set({ timeType: timeType.value })
+    })
+    hours.addEventListener("change", (event) => {
+        chrome.storage.sync.set({ hours: hours.value })
+    })
+    notes.addEventListener("change", (event) => {
+        chrome.storage.sync.set({ notes: notes.value })
+    })
+    overtimeType.addEventListener("change", (event) => {
+        chrome.storage.sync.set({ overtimeType: overtimeType.value })
+    })
+    compensationType.addEventListener("change", (event) => {
+        chrome.storage.sync.set({ compensationType: compensationType.value })
+    })
+    administrativeTime.addEventListener("change", (event) => {
+        chrome.storage.sync.set({ administrativeTime: administrativeTime.value })
+    })
+}
--- ts-autofiller/timesheets.js
+++ ts-autofiller/timesheets.js
@@ -1,7 +1,3 @@
-// 0 - project
-// 1 - task
-// 2 - time type
-
 function evaluateExternalArgs() {
     if (typeof globalArgs !== 'undefined') {
         return globalArgs
@@ -11,7 +7,10 @@ function evaluateExternalArgs() {
         task: "Billable",
         timeType: "Offshore",
         hours: 8,
-        administrativeTime: 0,
+        notes: "",
+        overtimeType: "None",
+        compensationType: "None",
+        administrativeTime: "None",
     }
 }
 
@@ -19,8 +18,12 @@ function evaluateExternalArgs() {
     const globalArgs = evaluateExternalArgs()
     console.log(globalArgs)
 
-    let selects = document.querySelectorAll(".timesheet .draggableColumns .timesheetControl select")
+    let emptyRow = document.querySelector(".gridDataEmptyRow")
+    let selects = emptyRow.querySelectorAll(".select-oa")
 
+    // 0 - project
+    // 1 - task
+    // 2 - time type
     for (let option of selects[0].querySelectorAll("option")) {
         if (option.innerText.startsWith(globalArgs.projectPrefix)) {
             selects[0].value = option.value
@@ -45,21 +48,52 @@ function evaluateExternalArgs() {
         }
     }
 
-    let hours = Array.from(document.querySelectorAll(".timesheetHours input")).slice(0, 5)
+    let hours = Array.from(emptyRow.querySelectorAll(".timesheetHours input")).slice(0, 5)
     for (let input of hours) {
         input.value = globalArgs.hours
     }
 
-    let noteAdd = Array.from(document.querySelectorAll(".noteAddSmall")).slice(0, 5)
+    let noteAdd = Array.from(emptyRow.querySelectorAll(".noteAddSmall")).slice(0, 5)
     for (let noteAddButton of noteAdd) {
         noteAddButton.click()
         let dialogSelect = Array.from(document.querySelectorAll(".dialogBlock select"))
-        let administrativeTime = dialogSelect[dialogSelect.length - 1]
+
+        let notes = document.querySelector(".dialogControls textarea")
+        notes.value = globalArgs.notes
+
+        let overtimeType = dialogSelect[0]
+        for (let option of overtimeType.querySelectorAll("option")) {
+            if (globalArgs.overtimeType === "None") {
+                overtimeType.value = "__BLANK"
+                break
+            }
+            if (option.innerText == globalArgs.overtimeType) {
+                overtimeType.value = option.value
+            }
+        }
+
+        let compensationType = dialogSelect[1]
+        for (let option of compensationType.querySelectorAll("option")) {
+            if (globalArgs.compensationType === "None") {
+                compensationType.value = "__BLANK"
+                break
+            }
+            if (option.innerText == globalArgs.compensationType) {
+                compensationType.value = option.value
+            }
+        }
+
+        let administrativeTime = dialogSelect[2]
         for (let option of administrativeTime.querySelectorAll("option")) {
+            if (globalArgs.administrativeTime === "None") {
+                administrativeTime.value = "__BLANK"
+                break
+            }
             if (option.innerText == globalArgs.administrativeTime) {
                 administrativeTime.value = option.value
             }
         }
+
         document.querySelector(".dialogOkButton").click()
     }
 })();
